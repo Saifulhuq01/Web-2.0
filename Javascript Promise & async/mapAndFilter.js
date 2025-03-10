7b2de9f997aef9d11e397ac678f47e294a320e2c
Saifulhuq <90808980+Saifulhuq01@users.noreply.github.com>
2025-03-10 07:35:51 +0530

    added map and filter sample

---
+++ Javascript Promise & async/mapAndFilter.js
@@ -0,0 +1,23 @@
+//"map" applies a function to every element in a collection, 
+// creating a new collection with the transformed elements, 
+// while "filter" selects only elements from a collection 
+// that meet a specific condition, 
+
+//map
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
+
+
+
+//filter
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
